b5454c7f214e95481406a5c0ef4f8dad76abf4b9
Danielle Neuberger <daneuber@microsoft.com>
2023-03-08 14:01:22 -0500

    repro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { 
+  DefaultButton, 
+  PrimaryButton, 
+  Dialog } from '@fluentui/react';
+import { useBoolean } from '@fluentui/react-hooks';
+
 function App() {
+  const [hideDialog, { toggle: toggleHideDialog }] = useBoolean(true);
+
+  const modalProps = {
+    titleAriaId: 'myLabelId',
+    subtitleAriaId: 'mySubTextId',
+    isBlocking: false,
+    styles: { main: { maxWidth: 850 } },
+  };
+
   return (
     <div className="App">
+      <div className='headerbtns'>
+          <DefaultButton text="outer button 1"/>
+          <DefaultButton text="outer button 2"/>
+          <DefaultButton text="outer button 3"/>
+          <DefaultButton text="outer button 4"/>
+          <DefaultButton text="outer button 5"/>
+        </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test focus trap with iframe modal
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <PrimaryButton text="open dialog" onClick={toggleHideDialog}/>
       </header>
+      <Dialog
+        hidden={hideDialog}
+        onDismiss={toggleHideDialog}
+        modalProps={modalProps}
+        >
+          <iframe src='../innerFrameContent.html' title='test iframe'/>
+      </Dialog>
     </div>
   );
 }
